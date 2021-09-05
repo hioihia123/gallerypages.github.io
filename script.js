a696f1af799debcb9965444561488660d347438c
Nguyen Phung <62664283+hioihia123@users.noreply.github.com>
2021-09-05 16:17:42 -0500

    GallerPages as requested
    As requested

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  for (var i=1; i <= $('.slider__slide').length; i++){
+    $('.slider__indicators').append('<div class="slider__indicator" data-slide="'+i+'"></div>')
+  }
+  setTimeout(function(){
+    $('.slider__wrap').addClass('slider__wrap--hacked');
+  }, 1000);
+})
+
+function goToSlide(number){
+  $('.slider__slide').removeClass('slider__slide--active');
+  $('.slider__slide[data-slide='+number+']').addClass('slider__slide--active');
+}
+
+$('.slider__next, .go-to-next').on('click', function(){
+  var currentSlide = Number($('.slider__slide--active').data('slide'));
+  var totalSlides = $('.slider__slide').length;
+  currentSlide++
+  if (currentSlide > totalSlides){
+    currentSlide = 1;
+  }
+  goToSlide(currentSlide);
+})
+$('.slider__previous, .go-to-previous').on('click', function(){
+  var currentSlide = Number($('.slider__slide--active').data('slide'));
+  var totalSlides = $('.slider__slide').length;
+  currentSlide--
+  if (currentSlide > totalSlides){
+    currentSlide = 1;
+  }
+  goToSlide(currentSlide);
+})
+
+
+
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
+
+
